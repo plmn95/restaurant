182d5a9d671caabf7a97cb59fb6213d15d6f081e
plmn95 <plmn9595@gmail.com>
2025-02-13 13:05:38 +0200

    first page done

--- src/tabAbout.js
+++ src/tabAbout.js
@@ -1,6 +1,8 @@
 import rectangle from './images/rectangle.png'
+import imgLineTop from './images/line-top.png'
+import imgLineBot from './images/line-bot.png'
 
-const renderAbout = function () {
+function renderAbout() {
     const container = document.getElementById('content')
 
     const divHeader = document.createElement('div')
@@ -15,10 +17,55 @@ const renderAbout = function () {
     subTitle.innerText = 'gourmet cuisine like you’ve never experienced before'
     subTitle.classList.add('sub-title')
     divHeader.appendChild(subTitle)
-    
+
     const banner = document.createElement('img')
     banner.src = rectangle
     container.appendChild(banner)
+
+    const divText = document.createElement('div')
+    divText.classList.add('divText')
+    container.appendChild(divText)
+
+    const text = document.createElement('p')
+    text.innerText = 'All-organic products, grown on-site in special biodomes,\nprepared with care and in accordance with\nthe latest Space health regulations'
+    divText.appendChild(text)
+
+    const divMiddle = document.createElement('div')
+    divMiddle.classList.add('divMiddle')
+    container.appendChild(divMiddle)
+
+    const lineTop = document.createElement('img')
+    lineTop.src = imgLineTop
+    divMiddle.appendChild(lineTop)
+
+    const hMiddle = document.createElement('h2')
+    hMiddle.classList.add('hMiddle')
+    hMiddle.innerText = '“A Celestial Culinary Experience”'
+    divMiddle.appendChild(hMiddle)
+
+    const lineBot = document.createElement('img')
+    lineBot.src = imgLineBot
+    divMiddle.appendChild(lineBot)
+
+    const divBot = document.createElement('div')
+    divBot.classList.add('divBot')
+    container.appendChild(divBot)
+
+    const pLocation = document.createElement('p')
+    pLocation.classList.add('pLocation')
+    pLocation.innerText = `You can find us on planet Teegarden b - Equatorial region
+
+                            Coordinates: RA 23h 59m 18.6496s | Dec +16° 52′ 53.251″
+                            System: Teegarden’s Star`
+    divBot.appendChild(pLocation)
+
+    const btnReserve = document.createElement('button')
+    btnReserve.classList.add('btnReserve')
+    btnReserve.innerText = 'Reserve a seat'
+    divBot.appendChild(btnReserve)
+
+    const line = document.createElement('hr')
+    container.appendChild(line)
 }
 
 export {
